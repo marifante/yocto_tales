d6a57e60477f23023924d904e3296b10bdee26ad
Julian <jnrodriguezz@hotmail.com>
2025-05-11 16:00:32 +0200

    add a command to know yoctales version

--- src/yoctales/cli.py
+++ src/yoctales/cli.py
@@ -3,6 +3,7 @@ Command Line Interface used to launch yoctales application.
 """
 import logging
 import argparse
+import pkg_resources
 
 from yoctales.linux_factory import create_linux_image
 
@@ -15,12 +16,22 @@ FORMAT = (
 
 log = logging.getLogger(__name__)
 
+def get_package_version(package_name: str) -> str:
+    """Retrieve the version of an installed pip package."""
+    try:
+        return pkg_resources.get_distribution(package_name).version
+    except pkg_resources.DistributionNotFound:
+        return f"Package {package_name} not found"
 
 def parse_args():
     """ Parse arguments given to this CLI. """
     parser = argparse.ArgumentParser(description="Application used to build linux images using yocto.")
 
-    parser.add_argument("--config", required=True, type=str, help="Yoctales config yml.")
+    # Ensure at least one group of arguments is provided
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--config", type=str, help="Yoctales config yml.")
+    group.add_argument("--version", default=False, action="store_true", help="Print yoctales installed version.")
+
     parser.add_argument("--log-level", default="INFO", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set logging level")
     parser.add_argument("--dry-run", default=False, action="store_true", help="Use this flag to only see the current plan that would be executed.")
 
@@ -35,7 +46,12 @@ def main():
 
     log.info("Starting yoctales CLI!")
 
-    create_linux_image(args.config, args.dry_run)
+    if args.version:
+        log.info(f"Yoctales version: {get_package_version('yoctales')}")
+    elif args.config:
+        create_linux_image(args.config, args.dry_run)
+    else:
+        raise ValueError(f"Unhandled case, at least args.version or args.config should be set.")
 
 
 if __name__ == "__main__":
